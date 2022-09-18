0bf1717067e882daab13f0f8880918cc10e7dbbc
Balaganesh003 <k.balaganesh26@gmail.com>
2022-09-18 15:53:35 +0530

    added serverless function to get coffeestores data

--- pages/index.js
+++ pages/index.js
@@ -38,12 +38,15 @@ export default function Home(props) {
     async function setCoffeeStoresByLocation() {
       if (latLong) {
         try {
-          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
-          console.log({ fetchedCoffeeStores });
+          const res = await fetch(`api/getCoffeeStoresByLocation?latLong=${latLong}&limit=30`);
+
+          const coffeeStores = await res.json();
+
+
           // setCoffeeStores(fetchedCoffeeStores);
           dispatch({
             type: ACTION_TYPES.SET_COFFEE_STORES,
-            payload: { coffeeStores: fetchedCoffeeStores },
+            payload: { coffeeStores: coffeeStores },
           });
         }
         catch (error) {
